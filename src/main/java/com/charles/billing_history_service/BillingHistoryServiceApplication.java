21de7505f19bcc06bab0caac76816ec26ccbfe7e
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 09:03:11 +0700

    [Charles] add initial spring boot project

---
+++ src/main/java/com/charles/billing_history_service/BillingHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.charles.billing_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingHistoryServiceApplication.class, args);
+	}
+
+}
