086ccc8f6d55005ea742dec2fa208aeead7692ad
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 12:30:27 +0700

    [Charles] register billing_history_service to eureka server

--- src/main/java/com/charles/billing_history_service/BillingHistoryServiceApplication.java
+++ src/main/java/com/charles/billing_history_service/BillingHistoryServiceApplication.java
@@ -2,8 +2,10 @@ package com.charles.billing_history_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BillingHistoryServiceApplication {
 
 	public static void main(String[] args) {
